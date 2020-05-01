321e3c1c473128b50274caf632d80bb9abafaea4
Mark Dow <dowmark4686@gmail.com>
2020-05-01 04:30:12 -0400

    Added collision at center of screen on click

---
+++ Assets/Scripts/Gunscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gunscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (Input.GetMouseButtonDown(0)) {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, fwd, out hit)){
+            print(hit.collider.name);
+            print("There is something in front of the object!");
+        }
+    
+    }
+    }
+}
