01814662ff298144afcad0a57dfe006bae171abc
Mark Dow <dowmark4686@gmail.com>
2020-05-02 19:49:20 -0400

    Added colliders to router and they get removed when shot. Added detection for when no more routers remain

--- Assets/Scripts/Gunscript.cs
+++ Assets/Scripts/Gunscript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Gunscript : MonoBehaviour
 {
+    public GameObject[] routers;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,16 +14,25 @@ public class Gunscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    if (Input.GetMouseButtonDown(0)) {
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, fwd, out hit)){
-            print(hit.collider.name);
-            hit.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
-            print("There is something in front of the object!");
+            if (Physics.Raycast(transform.position, fwd, out hit)){
+                print(hit.collider.name);
+                if(hit.transform.gameObject.tag == "Router"){
+                    Destroy(hit. transform. gameObject);
+                }
+                else{
+                    print("Didn't shoot router!");
+                }
+            }
+        
         }
-    
-    }
+        //routers
+        routers = GameObject.FindGameObjectsWithTag("Router");
+        if(routers.Length == 0){
+            print("All routers destroyed!");
+        }
     }
 }
