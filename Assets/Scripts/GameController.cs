5a43915eb5042fb96ace756bca531710c732240d
Jacob Calkins <jcalkins@umass.edu>
2020-05-04 17:39:33 -0400

    Beginnings of a damage system
    Currently gamecontroller implementation does not make sense

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public GameObject[] routers;
+    public GameObject[] tierIRouters;
+    public GameObject[] tierIIRouters;
+    public GameObject[] tierIIIRouters;
+    public GameObject[] tierIIIBeacons;
 
     public Text timerLabel;
     private float time;
@@ -13,6 +17,17 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        tierIRouters = GameObject.FindGameObjectsWithTag("Tier_1_Router");
+        tierIIRouters = GameObject.FindGameObjectsWithTag("Tier_2_Router");
+        tierIIIRouters = GameObject.FindGameObjectsWithTag("Tier_3_Router");
+        tierIIIBeacons = GameObject.FindGameObjectsWithTag("Tier_3_Beacon");
+
+        GameObject[][] routerList = { tierIRouters, tierIIRouters, tierIIIRouters, tierIIIBeacons };
+
+        for(int tier = 1; tier <= routerList.Length; tier++)
+            foreach(var router in routerList[tier-1]) {
+                DamageSystem newDamageSystem = new DamageSystem(tier);
+            }
         
     }
 
