b6997142119b9f60a1e1f53051e706f96ee0a007
Jacob Calkins <jcalkins@umass.edu>
2020-05-05 22:27:25 -0400

    Health system push 2
    The last push didnt seem to work

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,10 +6,11 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public GameObject[] routers;
-    public GameObject[] tierIRouters;
-    public GameObject[] tierIIRouters;
-    public GameObject[] tierIIIRouters;
-    public GameObject[] tierIIIBeacons;
+    private float projectileDamage = 30f;
+    // public GameObject[] tierIRouters;
+    // public GameObject[] tierIIRouters;
+    // public GameObject[] tierIIIRouters;
+    // public GameObject[] tierIIIBeacons;
 
     public Text timerLabel;
     private float time;
@@ -17,17 +18,17 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tierIRouters = GameObject.FindGameObjectsWithTag("Tier_1_Router");
-        tierIIRouters = GameObject.FindGameObjectsWithTag("Tier_2_Router");
-        tierIIIRouters = GameObject.FindGameObjectsWithTag("Tier_3_Router");
-        tierIIIBeacons = GameObject.FindGameObjectsWithTag("Tier_3_Beacon");
+        // tierIRouters = GameObject.FindGameObjectsWithTag("Tier_1_Router");
+        // tierIIRouters = GameObject.FindGameObjectsWithTag("Tier_2_Router");
+        // tierIIIRouters = GameObject.FindGameObjectsWithTag("Tier_3_Router");
+        // tierIIIBeacons = GameObject.FindGameObjectsWithTag("Tier_3_Beacon");
 
-        GameObject[][] routerList = { tierIRouters, tierIIRouters, tierIIIRouters, tierIIIBeacons };
+        // GameObject[][] routerList = { tierIRouters, tierIIRouters, tierIIIRouters, tierIIIBeacons };
 
-        for(int tier = 1; tier <= routerList.Length; tier++)
-            foreach(var router in routerList[tier-1]) {
-                DamageSystem newDamageSystem = new DamageSystem(tier);
-            }
+        // for(int tier = 1; tier <= routerList.Length; tier++)
+        //     foreach(var router in routerList[tier-1]) {
+        //         DamageSystem newDamageSystem = new DamageSystem(tier);
+        //     }
         
     }
 
@@ -41,7 +42,9 @@ public class GameController : MonoBehaviour
             if (Physics.Raycast(transform.position, fwd, out hit)){
                 print(hit.collider.name);
                 if(hit.transform.gameObject.tag == "Router"){
-                    Destroy(hit. transform. gameObject);
+                    HealthSystem routerHealth = hit.transform.GetComponent<HealthSystem>();
+                    routerHealth.DoDamage(projectileDamage);
+                    // Destroy(hit. transform. gameObject);
                 }
                 else{
                     print("Didn't shoot router!");
---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    private float health = 100f;
+
+    public void DoDamage(float damageAmount) {
+        health -= damageAmount;
+        if(health <= 0) Die();
+    }
+
+    void Die() {
+        // death animation
+        // let the game know an object has been removed
+        Destroy(gameObject);
+    }
+}
