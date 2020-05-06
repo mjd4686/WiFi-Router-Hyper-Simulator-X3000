f5bb970f4f8c1760d179520e8b6691548497f8f3
Jacob Calkins <jcalkins@umass.edu>
2020-05-05 22:36:51 -0400

    updated healthSystem to handle different router types
    Now toggleable in the inspector view to choose which router you want, this will give different damage values. Also, all routers in the scene now are assigned healthSystems.

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -4,7 +4,39 @@ using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-    private float health = 100f;
+    private float originalHealth;
+    private float health;
+    public int routerType; // 1 = Tier I, 2 = II, 3 = III hub, 4 = III beacon 
+
+    // values of the different routers
+    private float tier_i = 100f;
+    private float tier_ii = 500f;
+    private float tier_iii_h = 300f;
+    private float tier_iii_b = 150f;
+
+    void Start() {
+        switch (routerType) {
+            case 1:
+                originalHealth = tier_i;
+                health = tier_i;
+                break;
+            case 2:
+                originalHealth = tier_ii;
+                health = tier_ii;
+                break;
+            case 3:
+                originalHealth = tier_iii_h;
+                health = tier_iii_h;
+                break;
+            case 4:
+                originalHealth = tier_iii_b;
+                health = tier_iii_b;
+                break;
+            default:
+                // Debug.Log("Invalid router type");
+                break;
+        }
+    }
 
     public void DoDamage(float damageAmount) {
         health -= damageAmount;
