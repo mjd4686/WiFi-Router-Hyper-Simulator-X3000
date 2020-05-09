a2dda31c4f0194d94d157d6e32bc11b9a235b837
Jacob Calkins <jcalkins@umass.edu>
2020-05-08 23:36:40 -0400

    Added difficulty settings and updated the UI text

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+
+    // Difficulty level setting
+    private string[] difficulties ={"Easy", "Medium", "Hard"};
+    private string[] tooltips = {"Timer set to 2 minutes, half health on all routers", "Timer set to 1 minute, standard health on all routers", "Timer set to 45 seconds, router health increased by 25%"};
+    private string currentDifficulty;
+    private int d = 0; // rotates through difficulties by array index
+    public Text DifficultyLevel;
+    public Text DifficultyTooltip;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        currentDifficulty = difficulties[d];
+        DifficultyLevel.text = currentDifficulty;
+        DifficultyTooltip.text = tooltips[d];    
     }
 
     // Update is called once per frame
@@ -17,27 +28,32 @@ public class MainMenuScript : MonoBehaviour
         
     }
 
-    public void PlayGame(){
+    public void PlayGame() {
         SceneManager.LoadScene("Level1");
     }
     
-    public void Leaderboard(){
+    public void Leaderboard() {
         SceneManager.LoadScene("GameScene_HighscoreTable");
     }
     
-    public void Settings(){
+    public void Settings() {
         SceneManager.LoadScene("Settings");
     }
     
-    public void Back(){
+    public void Back() {
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void ChangeDifficulty(){
-        
+    public void ChangeDifficulty() {
+        if(d < 2) d++;
+        else d -= 2;
+        currentDifficulty = difficulties[d];
+
+        DifficultyLevel.text = currentDifficulty;
+        DifficultyTooltip.text = tooltips[d];
     }
 
-    public void ResetLeaderboard(){
-        
+    public void ResetLeaderboard() {
+        // do something @jerry
     }
 }
